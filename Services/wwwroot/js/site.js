d52df6e5f086b61a660d02c089d84e8e357f1560
Hemisol <58226093+Hemisol@users.noreply.github.com>
2019-11-26 22:33:59 -0800

    importing project

---
+++ Services/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$('#myCarousel').carousel();
+var winWidth = $(window).innerWidth();
+$(window).resize(function () {
+
+    if ($(window).innerWidth() < winWidth) {
+        $('.carousel-inner>.item>img').css({
+            'min-width': winWidth, 'width': winWidth
+        });
+    }
+    else {
+        winWidth = $(window).innerWidth();
+        $('.carousel-inner>.item>img').css({
+            'min-width': '', 'width': ''
+        });
+    }
+});
+
+
